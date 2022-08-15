4e01819ddfa21931da6679d330e3ca2fbfd0ac1a
Sondre Lindkjølen <47851430+sondredl@users.noreply.github.com>
2022-08-15 11:48:15 +0200

    buildall.bat builds and compiles correct paths

---
+++ source/helloworld.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+#include <cmath>
+
+int main()
+{
+    int a, b, c;
+
+    std::cout << "enter the coefficient of x-squared, x, and units: ";
+    std::cin >> a >> b >> c;
+
+    // double disc = (a * b) - (4 * a * c);
+    std::cout << "a + b + c = " << a + b + c << std::endl;
+    // if(disc < 0){
+    //     std::cerr << "Roots are imaginay " << std::endl;
+    // }
+    // else{
+        // if{
+        // double root1 = (-a + sqrt(disc)) / (2 * a);
+        // double root2 = (-a - sqrt(disc)) / (2 * a);
+
+
+        // std::cout << "Roots are " << root1 << " and " << root2 << std::endl;
+    // }
+    // return 0;
+}
