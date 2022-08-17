181a5224dd928ed968297fcf6284082de79d8f6d
Sondre Lindkjølen <47851430+sondredl@users.noreply.github.com>
2022-08-17 23:42:42 +0200

    tidy buildAll.sh

--- buildAll.sh
+++ buildAll.sh
@@ -1,9 +1,5 @@
 #!/bin/bash
 
-#rm -rf build/*
-#rm o/main.exe
-
-#cmake ../CMakeLists.txt
 cmake . -B build -GNinja 		# builds .o files in build/
 cmake --build build --config Release 	# compiles .o files in build/ to specified folder in CMakeLists
 
